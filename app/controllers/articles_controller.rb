8478ff51e9333713230612f66cad8d27f15a8972
BetimShala <betim_sh97@live.com>
2017-08-19 11:37:56 +0200

    Add precompiled assets for Heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,7 +62,6 @@ class ArticlesController < ApplicationController
   private 
   def find_article
     @article=Article.find(params[:id])
-    
   end
   def article_params
     params.require(:article).permit(:title,:description)
